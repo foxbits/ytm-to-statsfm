cab7802bacbe8d1cffcfe5408aaaaffa9fa2b196
foxbits <marian@foxbites.net>
2025-09-05 23:26:00 +0300

    Update the list of video tags from ytm videos

--- sanitizer.py
+++ sanitizer.py
@@ -24,7 +24,7 @@ def sanitize_video_track_info(track_name: str, artist_name: str) -> tuple[str, s
     # Remove official tags from title
     officialTagsStripping = [
         r"([-|]\s*)?\((?:Official.*)\)(?:\s*[-|])?",
-        r"([-|]\s*)?\(?(?:Official Video|Official Audio|Official)\)?(?:\s*[-|])?"
+        r"([-|]\s*)?\(?(?:Official Music Video|Official Lyric Video|Official Visual Video|Official Video|Official Audio|Official Track|Official|Videoclip Oficial|Videoclip)\)?(?:\s*[-|])?"
     ]
     for pattern in officialTagsStripping:
         track_name = re.sub(pattern, "", track_name, flags=re.IGNORECASE).strip()
